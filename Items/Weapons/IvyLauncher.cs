9b7c0cf4fc9c7e091ed3d0a1f251813b431658c2
Almar Mulder <info@lionholm.com>
2020-06-18 17:47:32 +0200

    Updated description

--- Items/Weapons/IvyLauncher.cs
+++ Items/Weapons/IvyLauncher.cs
@@ -39,7 +39,7 @@ namespace CommunityPack.Items.Weapons
             item.shootSpeed = 15f;
             item.noMelee = true;
             item.ranged = true;
-            item.useAmmo = ItemType<VineBallAmmo>();
+            item.useAmmo = ItemType<Ammo.VineBallAmmo>();
         }
 
         public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
